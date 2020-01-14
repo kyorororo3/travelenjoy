8658e1dac4550fe4a410a6da863180576aea05ff
YEONHA KIM <yeonha9403@gmail.com>
2020-01-14 15:57:39 +0900

    ++ node 서버 tour 라우터 생성 ++ node 서버 tour list 조회코드 작성

---
+++ server/routes/tour.js
@@ -0,0 +1,22 @@
+// EXPRESS
+const express = require('express');
+const router = express.Router();
+
+// MODULES
+const bodyParser = require('body-parser');
+
+// DB CONNECTION
+const dbconfig = require('../config/dbconfig')();
+const conn = dbconfig.init();
+dbconfig.conn_test(conn);
+
+router.get('/list', function (req, res) {
+  var sql = "select * from te_tour";
+  conn.query(sql, function (err, rows) {
+    if(err) return console.log("ERR!! " + err);
+    console.log(rows);
+    res.send(rows);
+  })
+});
+
+module.exports = router;

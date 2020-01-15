51e6aa33b79dbe0a5afd68584f0bbcaffc391976
Yeonha Kim <younger9403@gmail.com>
2020-01-15 17:39:29 +0900

    ++ fetch를 이용해 전송된 투어 디테일 정보들 가져옴

--- src/components/travel/traveldetail.js
+++ src/components/travel/traveldetail.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 class Tourdetail extends React.Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded : false,
+      tour_info : null,
+      tour_sche : null,
+      tour_des : null
+    }
+  }
+
+  componentDidMount() {
     let seq = this.props.match.params.seq;  // url path로 보내진 파라미터 받아옴.
 
+    fetch('http://localhost:3002/tour/detail/' + seq)
+      .then( res => res.json() )
+      .then( data => this.setState({
+        isLoaded: true,
+        tour_info: data.tour_info,
+        tour_sche: data.tour_sche,
+        tour_des: data.tour_des
+      }) );
+  }
+
+  render() {
     return(
       <div className='tour-detail-wrapper'>
         <h2>detail component</h2>
-        {seq}
+        
       </div>
     )
   }

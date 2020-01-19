09f523fd02f758f11182a995fe76946964c3f51c
HyeongJunMin <alsgudwns007@naver.com>
2020-01-19 17:14:47 +0900

    기본 quill 에디터 스킨 적용

--- src/components/freetalk/TalkWrite.js
+++ src/components/freetalk/TalkWrite.js
@@ -1,8 +1,20 @@
 import React, {Component} from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 //새 게시물 작성
 
 class TalkWrite extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { text: '' } // You can also pass a Quill Delta here
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(value) {
+        this.setState({ text: value })
+    }
 
     componentDidMount() {
 
@@ -16,6 +28,8 @@ class TalkWrite extends Component {
         return (
             <div className="talk-write-wrap">
                 <p>게시물 작성합시다 ㅎㅎ</p>
+                <ReactQuill value={this.state.text}
+                            onChange={this.handleChange} />
             </div>
         );
     }

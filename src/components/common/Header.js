b3e3c900c8ee957b10995ff99eb18fe3af261398
kyorororo3 <kyorororo3@gmail.com>
2020-01-20 14:42:40 +0900

    2020.01.20 마이페이지 폼 만들고 푸시

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -28,6 +28,7 @@ class Header extends React.Component {
                                 <li><NavLink exact to='/' className='menu' activeClassName='active'>Home</NavLink></li>
                                 <li><NavLink to='/travel' className='menu'>Travel</NavLink></li>
                                 <li><NavLink to='/talk' className='menu'>Talk</NavLink></li>
+                                <li><NavLink to='/mypage' className='menu'>Mypage</NavLink></li>
                                 <li className='login-btn'><NavLink to='/login'><span>Login</span></NavLink></li>
                             </ul>
                         </div>
--- src/components/mypage/MypageHome.js
+++ src/components/mypage/MypageHome.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import '../../resources/mypage/css/mypage.css';
+import profile_img from '../../resources/mypage/images/profile_img.jpg';
 
 class MypageHome extends Component {
 
@@ -8,7 +10,46 @@ class MypageHome extends Component {
     }
     render(){
         return(
-            <div>my Component</div>
+            <div className='container'>
+                <div className='mypage-heading'>마이페이지 홈</div>
+                <div className='mypage-sidebar'>
+                    <div className='sidebar-wrapper'>
+                        <div className='profile-container box'>
+                            <div className='profile-img-wrapper'>
+                                <img className ='profile-img' src={profile_img}/>
+                            </div>
+                            <div className='profile-info'>
+                                Profile info will be here 
+                            </div> 
+                        </div>
+                        <div className='item-container box'>
+                            <div className='item'>
+                                <Link to='/mypage/schedule'>My Schedule</Link>
+                            </div>
+                            <div className='item'>
+                                <Link to='/mypage/travel'>My Travel</Link>
+                            </div>
+                            <div className='item'>
+                                <Link to='/mypage/wishlist'>My Wishlist</Link>
+                            </div>
+                            <div className='item'>
+                                 <Link to='/mypage/history'>My History</Link>
+                            </div>
+                            <div className='item'>
+                                <Link to='/mypage/info'>My Info</Link>
+                            </div>
+                        </div>            
+                    </div>
+                </div>
+                <div className='mypage-body'>
+                    <div className='body-wrapper box'>
+                        <div className='body-info-container'> 
+                            Mypage contents(body) will be here
+
+                        </div>
+                    </div>
+                </div>         
+            </div>
         );
     }
 }

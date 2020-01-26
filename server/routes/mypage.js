aea75fa6afdc3ffa1dbd97058538407362f4c990
kyorororo3 <kyorororo3@gmail.com>
2020-01-26 11:25:25 +0900

    2020-01-26 마이페이지 계속해서 수정 중

--- server/routes/mypage.js
+++ server/routes/mypage.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest:'uploads/'})
+
 
 let mysql_dbc = require('../config/dbconfig')();
 let connection = mysql_dbc.init();
 mysql_dbc.conn_test(connection);
 
+
 router.use(bodyParser.json());
 
+router.post('/info/nickname', (req,res)=>{
+    let input_nickname = req.body;
+    let sql = 'select * from te_member where nickname=?';
 
+    connection.query(sql, input_nickname, (err, result) => {
+        if(err) return console.log('err' + err);
+        console.log(result);
+        if(result.length === 0){
+            console.log('사용할 수 있는 닉네임입니다.');
+            res.send({isExistNickname:false});
+        }else{
+            console.log('이미 존재하는 닉네임입니다.');
+            res.send({isExistNickname:true});
+        }
+
+    })
+    
+});
 module.exports = router;
+
+
--- src/components/mypage/MyInfo.js
+++ src/components/mypage/MyInfo.js
@@ -6,35 +6,58 @@ class MyInfo extends Component {
     constructor(props){
         super(props);
 
+        this.state = {
+            isExistNickname : ''
+        }
+
         this.editInfoHanlder = this.editInfoHanlder.bind(this);
+        this.saveEditedInfoHandler = this.saveEditedInfoHandler.bind(this);
+        this.checkNicknameHandler = this.checkNicknameHandler.bind(this);
     }
 
     editInfoHanlder(e){
         e.preventDefault();
-        let inputs = document.getElementsByClassName('form-input-group');
-
+        let inputs = document.getElementsByClassName('editable');
+        
         for (let index = 0; index < inputs.length; index++) {
-            inputs[index].readOnly = false;
+            inputs[index].removeAttribute('readOnly');
+        }
+        
+        document.getElementById('duplicate-check').style.display = 'block';
+        document.getElementById('editBtn').style.display = 'none';
+      
+        document.getElementById('cancelBtn').style.display = 'inline-block';
+        document.getElementById('saveBtn').style.display = 'inline-block';
+        
+    }
+
+    saveEditedInfoHandler(e){
+        e.preventDefault();
+
+    }
+
+    checkNicknameHandler(e){
+        e.preventDefault();
+        const input_nickname = document.getElementsByName('nickname')[0].value;
+        
+        fetch('http://localhost:3002/mypage/info/nickname',{
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ nickname: input_nickname })
+        })
+        .then(res => res.json())
+        .then (data => this.setState({
+          isExistNickname:  data.isExistNickname
+          
+        })
+        
+        );
+        if(this.state.isExistNickname === true){
+            alert('이미 존재하는 닉네임입니다.');
+        }else{
+            alert('사용할 수 있는 닉네임입니다.');
         }
 
-        document.getElementById('editBtn').style.display = 'none';
-        let cancelBtn = document.createElement('button');
-        let saveBtn = document.createElement('button');
-
-        let btn_wrapper = document.getElementById('btn-wrapper');
-
-        cancelBtn.id = 'cancelBtn';
-        cancelBtn.className = 'update-group-btn';
-        cancelBtn.onClick='cancelEditedInfoHandler';
-        cancelBtn.innerHTML = 'CANCEL';
-
-        saveBtn.id = 'saveBtn';
-        saveBtn.className = 'update-group-btn';
-        cancelBtn.onClick='saveEditedInfoHandler';
-        saveBtn.innerHTML = 'SAVE';
-
-        btn_wrapper.appendChild(cancelBtn);
-        btn_wrapper.appendChild(saveBtn);
     }
 
     render(){
@@ -53,23 +76,30 @@ class MyInfo extends Component {
                             </div>
                         </div>
                         <div className='user-info-container'>
-                            <form id='user-info-edit-form' >
-                                <div className='form-group'>
-                                    <div className='info-title'>Name</div>
-                                    <div className='input-wrapper'>
-                                        <input className='form-input-group' readOnly type='text' name='name' value={this.props.location.state.users.name} />
-                                    </div>
-                                </div>
+                            <form id='user-info-edit-form' action='/mypage/info/edit' encType='multipart/form-data'>
                                 <div className='form-group'>
                                     <div className='info-title'>Email</div>
                                     <div className='input-wrapper'>
-                                        <input className='form-input-group' readOnly type='text' name='email' value={this.props.location.state.users.email} />
+                                        <input className='form-input-group' readOnly type='text' name='email' defaultValue={this.props.location.state.users.email} />
+                                    </div>
+                                </div>
+                                <div className='form-group'>
+                                    <div className='info-title'>Name</div>
+                                    <div className='input-wrapper'>
+                                        <input className='form-input-group ' readOnly type='text' name='name' defaultValue={this.props.location.state.users.name} />
+                                    </div>
+                                </div>
+                                <div className='form-group'>
+                                    <div className='info-title'>Nickname</div>
+                                    <div className='input-wrapper'>
+                                        <input className='form-input-group editable' readOnly  type='text' name='nickname' defaultValue={this.props.location.state.users.nickname} />
+                                        <a id='duplicate-check' onClick={this.checkNicknameHandler}>duplicate check</a>
                                     </div>
                                 </div>
                                 <div className='form-group'>
                                     <div className='info-title'>Phone</div>
                                     <div className='input-wrapper'>
-                                        <input className='form-input-group' readOnly type='text' name='phone' value={this.props.location.state.users.phone} />
+                                        <input className='form-input-group editable' readOnly type='text' name='phone' defaultValue={this.props.location.state.users.phone} />
                                     </div>
                                 </div>
 
@@ -80,7 +110,8 @@ class MyInfo extends Component {
                                     </div>
                                 </div>
                                 <div id='btn-wrapper'>
-
+                                    <button id='cancelBtn' className='update-group-btn' onClick={this.cancelEditedInfoHander}>CANCEL</button>
+                                    <button id='saveBtn' className='update-group-btn' onClick={this.saveEditedInfoHandler}>SAVE</button>
                                 </div>
                             </form>
                         </div>

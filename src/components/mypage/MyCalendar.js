388c1ea8d122fc54327dda2e5137c057138150e4
kyorororo3 <kyorororo3@gmail.com>
2020-02-05 16:07:44 +0900

    2020-02-05 일본 가기전에 작업했던 부분

--- src/components/mypage/MyCalendar.js
+++ src/components/mypage/MyCalendar.js
@@ -7,17 +7,29 @@ import '../../resources/mypage/css/mycalendar.scss';
 
 class MyCalendar extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-    }
-
+    
+        this.state = {
+          email:this.props.location.state.users.email,
+          list: []
+        }
+      }
+      componentDidMount() {
+        fetch(`http://localhost:3002/mypage/travel?email=${this.state.email}`)
+          .then(res => res.json())
+          .then(data => this.setState({
+            list: data
+            })
+          );
+      }
     render(){
         return(
             <div className='mypage-body'>
                     <div className='body-wrapper box'>
                         <div className='body-info-container'> 
                            <div className='calendar-wrapper'>
-                                <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+                                <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]}    events={this.state.list} />
                            </div>
                         </div>
                     </div>

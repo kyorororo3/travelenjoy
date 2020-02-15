6c361b1480bb5ff8d6121612a902c928b3c1d8ee
jiji_eun <dis2s2@naver.com>
2020-02-15 23:38:44 +0900

    guide_q&a

--- src/components/guide/guide_question_obj.js
+++ src/components/guide/guide_question_obj.js
@@ -25,14 +25,22 @@ class Guide_Question_Obj extends React.Component {
     const {writer_info, isLoaded} = this.state
 
     return(
-      <div className='question-obj-wrapper' data-seq={seq} onClick={this.props.handleClick}>
-        <div className='client-profile-img' data-seq={seq}>
-          {isLoaded && <img data-seq={seq} src={require(`../../uploads/${writer_info.profile_img}`)}/>}
-        </div>
-        <div className='client-nickname' data-seq={seq}>
-          {isLoaded && writer_info.nickname}
-        </div>
-      </div>
+      // <div className='question-obj-wrapper' data-seq={seq} onClick={this.props.handleClick}>
+      //   <div className='client-profile-img' data-seq={seq}>
+      //     {isLoaded && <img data-seq={seq} src={require(`../../uploads/${writer_info.profile_img}`)}/>}
+      //   </div>
+      //   <div className='client-nickname' data-seq={seq}>
+      //     {isLoaded && writer_info.nickname}
+      //   </div>
+      // </div>
+
+        <tbody>
+        <tr>
+          <td data-seq={seq}>{seq}</td>
+          <td data-seq={seq}>{isLoaded && writer_info.nickname}</td>
+          <td><input type="button" value='chat' className='qa-button' data-seq={seq} onClick={this.props.handleClick}></input></td>
+        </tr>
+      </tbody>
     )
   }
 }

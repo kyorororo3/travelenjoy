99ab7d4a5d0b811d611afbe7acf11a3db95f5292
younger9403 <younger9403@gmail.com>
2020-02-17 15:16:51 +0900

    ++ 취소버튼 추가 : 지난 투어는 취소버튼 나타나지 않도록 제어

--- src/components/mypage/UI/ResModal.js
+++ src/components/mypage/UI/ResModal.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 
+// Utility
+import {stringToDate} from '../../../utils/Functions';
+
 class ResModal extends Component {
 
 
     constructor(props){
         super(props);
-
+        
+        const {start_date} = this.props.tour;
+        let _after = false;
+        if(stringToDate(start_date) < new Date()) _after = true;
+        
         this.state = {
-
+          after: _after,
         }
 
     }
 
     closeBtnHandler = (e) => {this.props.callbackFromParent({showResModal:false, tour:[]})}
     
+    cancleBtnHandler = (e) => {
+      alert("cancle button");
+    }
+    
  
     render(){
 
         const {category, title, reservation_number, start_date, join_people, total_price, phone, message} = this.props.tour;
-      
+        const {after} = this.state;
+
         return(
             <div className='modal-wrapper'>
                 <div className='modal-header'>
@@ -49,6 +61,7 @@ class ResModal extends Component {
                     <input className='col-sm-7 reservation-info-readonly' type='text' value={message? message:'N/A'} readOnly/>
                 </div>       
                 <div className='modal-footer'>
+                    {!after && <button type='button' className='btn-primary' onClick={this.cancleBtnHandler}>CANCLE</button>}
                     <button type='button' className='btn-primary' onClick={this.closeBtnHandler}>CLOSE</button>
                 </div>
             </div>

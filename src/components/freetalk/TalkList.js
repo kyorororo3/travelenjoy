c2da25d83d93f5395bbf241d0c028e0fdc9b7590
HyeongJunMin <alsgudwns007@naver.com>
2020-02-22 22:00:52 +0900

    검색완료

--- src/components/freetalk/TalkList.js
+++ src/components/freetalk/TalkList.js
@@ -1,19 +1,31 @@
 import React, {Component} from "react";
 import Talk from "./Talk";
+import TalkSearch from "./TalkSearch";
 
 //게시물 리스트
 
 class TalkList extends Component {
 
-    state = {
-        talkLength: 9,
-        list: [],
-        currentUser: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            talkLength: 9,
+            list: [],
+            currentUser: '',
+            keyword: ''
+        }
     }
 
+
     componentDidMount() {
+        // alert('키워드 : ' + this.props.keyword)
         window.addEventListener("scroll", this.handleScroll);
-        fetch('http://localhost:3002/freetalk/list')
+        // fetch('http://localhost:3002/freetalk/list?keyword=' + this.state.keyword)
+        this.setTalk(this.state.keyword)
+    }
+
+    setTalk = (keyword) => {
+        fetch('http://localhost:3002/freetalk/list?keyword=' + keyword)
             .then(res => res.json())
             .then(res => this.setState({list: res.list}))
             .then(res => this.setState({talkLength: this.state.list.length}))
@@ -26,7 +38,7 @@ class TalkList extends Component {
     }
 
     addTalk = (data) => {
-        fetch('http://localhost:3002/freetalk/list?seq=' + (this.state.talkLength + 1))
+        fetch('http://localhost:3002/freetalk/list?seq=' + (this.state.talkLength + 1) + '&keyword=' + this.state.keyword)
             .then(res => res.json())
             .then(res => this.setState({list: this.state.list.concat(res.list)}))
             .then(res => this.setState({talkLength: this.state.list.length}))
@@ -40,9 +52,18 @@ class TalkList extends Component {
             document.body.scrollTop;
         if (scrollHeight - innerHeight - scrollTop < 100) {
             console.log("Almost Bottom Of This Browser");
-            fetch('http://localhost:3002/freetalk/list?seq=' + (this.state.talkLength + 1))
+            fetch('http://localhost:3002/freetalk/list?seq=' + (this.state.talkLength + 1) + '&keyword=' + this.state.keyword)
                 .then(res => res.json())
-                .then(res => this.setState({list: this.state.list.concat(res.list)}))
+                .then(res => {
+                    if(res.list === undefined){
+                        console.log('list undefined!')
+                        return;
+                    }
+                    console.log(JSON.stringify(res.list))
+                    console.log('list isnot undefined! ' + this.state.talkLength)
+                    this.setState({list: this.state.list.concat(res.list)})
+                    this.setState({talkLength: this.state.list.length})
+                })
                 .then(res => this.setState({talkLength: this.state.list.length}))
         }
     };
@@ -56,9 +77,16 @@ class TalkList extends Component {
             this.setState({list: this.state.list.splice(1, (this.state.list.length -1) )})
     }
 
+    handleSearch = (keyword) => {
+        console.log('keyword : ' + keyword)
+        this.setState({keyword: keyword});
+        this.setTalk(keyword);
+    }
+
     render() {
         return (
             <div className="talk-list-wrap">
+                <TalkSearch handleSearch={this.handleSearch}/>
                 {this.state.currentUser}
                 {(this.state.list != null)?
                     this.state.list.map( (talk, i) => (
--- src/components/freetalk/TalkSearch.js
+++ src/components/freetalk/TalkSearch.js
@@ -4,10 +4,32 @@ import React, {Component} from "react";
 
 class TalkSearch extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            keyword : (props.keyword === '')?'':props.keyword
+        }
+    }
+
+    handleOnchange = (e) => {
+        e.preventDefault();
+        this.props.handleSearch(e.target.value);
+    }
+
     render() {
         return (
             <div className="talk-search-wrap">
                 <div className="main-text">이야기를 검색해 보세요.</div>
+                <div className='talk-search-form-wrap'>
+                    <form className='talk-search-form' action='/talk' autoComplete='off'
+                          onSubmit={this.handleSearch}>
+                        <input className='talk-search-input' type='text' name='keyword'
+                               placeholder='검색어 입력' value={this.state.keyword}
+                               onChange={this.handleOnchange}
+                        />
+                        <input className='btn-search' type='submit' value=''/>
+                    </form>
+                </div>
             </div>
         );
     }

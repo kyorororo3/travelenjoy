d898dd7557c70bf46bf1305349f31dff7ed0cce1
kyorororo3 <kyorororo3@gmail.com>
2020-02-24 22:20:28 +0900

    2020-02-24 myHome 에서 좋아요 / 댓글이 달렸을 때 메세지 오는 처리 완료

--- server/routes/mypage.js
+++ server/routes/mypage.js
@@ -46,8 +46,7 @@ router.get('/home', (req, res)=>{
 router.get('/home/msg', function(req,res){
     const {email} = req.query;
     let param = [email];
-    let sql = 'select * from te_tour a, te_tour_reservation b, te_tour_msg c '
-            + ' where a.seq = b.tour_seq and a.seq = c.tour_seq and b.email = ? '
+    let sql = 'select * from te_freetalk_msg where recipient = ? order by msg_reg_date desc';
     
     sql = mysql.format(sql, param);
 
@@ -57,6 +56,37 @@ router.get('/home/msg', function(req,res){
         res.send(rows);
     });
 });
+
+//myHome 에서 msg 읽음 처리
+router.get('/home/msg/update', function(req,res){
+    const {seq} = req.query;
+    let param = [seq];
+    let sql = 'update te_freetalk_msg set isread = 1 where msg_seq = ?';
+    
+    sql = mysql.format(sql, param);
+
+    console.log(sql);
+    connection.query(sql,  function (err, rows) {
+        if(err) return console.log("ERR!! " + err);
+        res.send(rows);
+    });
+});
+
+//myHome 에서 msg 읽음 처리
+router.get('/home/msg/delete', function(req,res){
+    const {seq} = req.query;
+    let param = [seq];
+    let sql = 'delete from te_freetalk_msg where msg_seq = ?';
+    
+    sql = mysql.format(sql, param);
+
+    console.log(sql);
+    connection.query(sql,  function (err, rows) {
+        if(err) return console.log("ERR!! " + err);
+        res.send(rows);
+    });
+});
+
 
 //MyInfo 닉네임 중복체크
 router.post('/info/nickname', (req,res)=>{
@@ -257,7 +287,7 @@ router.get('/review', (req, res)=>{
         sql = 'select c.*, d.seq as review_seq, d.email, d.title as review_title, d.content as review_content, d.score, d.review_img' 
             +' from (select a.seq, a.category, a.thumbnail, a.price, a.title, b.seq as res_seq, '
             +' b.email, b.start_date from te_tour a, te_tour_reservation b where a.seq = b.tour_seq) as c left join '
-            +' te_tour_review as d on c.res_seq = d.res_seq where c.email =? and c.start_date < now() order by c.start_date asc limit ?, 3';
+            +' te_tour_review as d on c.res_seq = d.res_seq where c.email =? and c.start_date < now() and d.res_seq is null order by c.start_date asc limit ?, 3';
         params = [email, parseInt(currentPage)];
         sql = mysql.format(sql,params);
         console.log(sql);
@@ -306,7 +336,7 @@ router.get('/review/length', function(req,res){
 
 //MyReview 추가 
 router.post('/review/register', upload.single('review_img'), function(req,res){
-    const { tour_seq, email, title, content, score} = req.body;
+    const { tour_seq, res_seq, email, title, content, score} = req.body;
     console.log('file 체크' , req.file)
     let review_img = '';
     if(req.file === undefined){
@@ -315,8 +345,8 @@ router.post('/review/register', upload.single('review_img'), function(req,res){
         review_img = req.file.filename;
         console.log('review_img 파일명 체크 ', review_img)
     }
-    let sql = 'insert into te_tour_review values(null,?,?,?,?,?,?,now())';
-    let params = [tour_seq, email, title, content, score, review_img];
+    let sql = 'insert into te_tour_review values(null,?,?,?,?,?,?,?,now())';
+    let params = [tour_seq, res_seq, email, title, content, score, review_img];
     sql = mysql.format(sql,params);
 
     console.log(sql);
